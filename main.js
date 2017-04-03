36fcd149afff99504acf43551c8ebf1c99bcc2ba
Conrado Quilles Gomes <conradoqg@gmail.com>
2017-04-03 20:44:20 -0300

    Adds command-line options to choose ports and peers.

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
-const httpServer = require('./lib/server');
-const blockchain = require('./lib/blockchain');
-const peerToPeer = require('./lib/p2p');
+const HttpServer = require('./lib/server');
+const Blockchain = require('./lib/blockchain');
+const PeerToPeer = require('./lib/p2p');
 
-peerToPeer.bootstrap(blockchain);
-httpServer.bootstrap(peerToPeer, blockchain);
+const argv = require('yargs')
+    .usage('Usage: $0 [options]')
+    .alias('p', 'http-port')
+    .alias('n', 'p2p-port')
+    .describe('p', 'HTTP port.')
+    .describe('n', 'P2P port.')
+    .describe('peers', 'Peers list.')
+    .array('peers')    
+    .help('h')
+    .alias('h', 'help')
+    .argv;
+
+let httpPort = process.env.PORT || process.env.HTTP_PORT || argv.httpPort || 3001;
+let p2pPort = process.env.P2P_PORT || argv.p2pPort || 6001;
+let peers = (process.env.PEERS ? process.env.PEERS.split(',') : argv.peers || []);
+
+let blockchain = new Blockchain();
+let peerToPeer = new PeerToPeer(p2pPort, peers, blockchain);
+let httpServer = new HttpServer(httpPort, peerToPeer, blockchain);
