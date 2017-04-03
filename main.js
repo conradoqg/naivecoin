dbfe24862901777a16c4391af18a468cd4825b6d
Conrado Quilles Gomes <conradoqg@gmail.com>
2017-04-03 15:57:31 -0300

    Reformats code.

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
-'use strict'
+const httpServer = require('./lib/server');
+const blockchain = require('./lib/blockchain');
+const peerToPeer = require('./lib/p2p');
 
-const httpServer = require('./lib/server')
-const blockchain = require('./lib/blockchain')
-const peerToPeer = require('./lib/p2p')
-
-peerToPeer.bootstrap(blockchain)
-httpServer.bootstrap(peerToPeer, blockchain)
+peerToPeer.bootstrap(blockchain);
+httpServer.bootstrap(peerToPeer, blockchain);
