05163e7f0c3e81bd9d32efb1fb45c15c34ec5f64
Conrado Quilles Gomes <conradoqg@gmail.com>
2017-04-06 20:14:42 -0300

    Replaces the direct saving of blockchain to disk by a repository class.

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const HttpServer = require('./lib/server');
 const Blockchain = require('./lib/blockchain');
 const PeerToPeer = require('./lib/p2p');
+const BlockChainRepository = require('./lib/repository/blockchain');
 
 const argv = require('yargs')
     .usage('Usage: $0 [options]')
@@ -20,6 +21,7 @@ let p2pPort = process.env.P2P_PORT || argv.p2pPort || 6001;
 let peers = (process.env.PEERS ? process.env.PEERS.split(',') : argv.peers || []);
 let name = process.env.NAME || argv.name || '1';
 
-let blockchain = new Blockchain(name);
+let repository = new BlockChainRepository(name);
+let blockchain = new Blockchain(repository);
 let peerToPeer = new PeerToPeer(p2pPort, peers, blockchain);
 let httpServer = new HttpServer(httpPort, peerToPeer, blockchain);
