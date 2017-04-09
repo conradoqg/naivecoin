bd9b805bc13a66020ac764fce43be743efdbbf9e
Conrado Quilles Gomes <conradoqg@gmail.com>
2017-04-09 09:59:10 -0300

    Adds swagger documentation and improves peers identification.

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ const BlockChainRepository = require('./lib/repository/blockchain');
 
 const argv = require('yargs')
     .usage('Usage: $0 [options]')
-    .alias('p', 'http-port')    
-    .describe('p', 'HTTP port.')    
+    .alias('p', 'http-port')
+    .describe('p', 'HTTP port.')
     .describe('peers', 'Peers list.')
     .describe('name', 'Peers list.')
     .array('peers')
@@ -16,6 +16,7 @@ const argv = require('yargs')
 
 let httpPort = process.env.PORT || process.env.HTTP_PORT || argv.httpPort || 3001;
 let peers = (process.env.PEERS ? process.env.PEERS.split(',') : argv.peers || []);
+peers = peers.map((peer) => { return { url: peer }; });
 var name = process.env.NAME || argv.name || '1';
 
 require('./lib/util/consoleWrapper.js')(name);
