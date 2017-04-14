388da92b44a0489ddd23af3a715828cd17359398
Conrado Quilles Gomes <conradoqg@gmail.com>
2017-04-14 11:50:50 -0300

    Removes repository concept, classes partially done and adds crypto util.

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
 const HttpServer = require('./lib/server');
 const Blockchain = require('./lib/blockchain');
 const Node = require('./lib/node');
-const Operator = require('./lib/operator');
-const BlockChainRepository = require('./lib/repository/blockchain');
-const OperatorRepository = require('./lib/repository/operator');
 
 const argv = require('yargs')
     .usage('Usage: $0 [options]')
@@ -23,11 +20,7 @@ var name = process.env.NAME || argv.name || '1';
 
 require('./lib/util/consoleWrapper.js')(name);
 
-let blockchainRepository = new BlockChainRepository(name);
-let operatorRepository = new OperatorRepository(name);
-
-let blockchain = new Blockchain(blockchainRepository);
+let blockchain = new Blockchain(name);
 let node = new Node(httpPort, peers, blockchain);
-let operator = new Operator(operatorRepository, blockchain);
 
-let httpServer = new HttpServer(httpPort, node, blockchain, operator);
+let httpServer = new HttpServer(httpPort, node, blockchain);
