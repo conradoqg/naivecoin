1dd90667c2b08f05b270a30e2aa2aca7f0a76ce8
Eupharia113 <aria@euphmail.com>
2018-10-15 03:19:39 +0100

    Mining interface revamp, notification upgrade, other minor bugfixes and additions
    Notifications are now sent as Windows Toast notifications, and many in-wallet notifications have been moved onto the new notification system, leading to a much cleaner in-wallet interface and an intuitive notification feed for Transactions/Warnings/Messages.
    
    Default values have been mostly replaced with 'placeholder' HTML values, a much more user-friendly input hint.
    
    Decreased blocks until difficulty changes for easier difficulty testing.
    
    New host detection and formatting upon the local console and via REST P2P data exchange, easier to use when running a node on localhost.
    
    Fixed a multitude of bugs in the wallet GUI and node-based code.
    
    Found the exact origin of the Mining Memory Leak, next commit may have a fix for said issue.

--- launch.bat
+++ launch.bat
@@ -1,7 +1,5 @@
 @echo off
 title Concord Core Node Setup
-echo Welcome to the Dev-Build of Concord Core. Please input your machine's IP address below, your node will be ran on this and will be shared among it's peers.
-echo WARNING - If you are running an offline node, or simply haven't port-forwarded, please type "localhost" and hit Enter.
 REM set /p ip="Enter IP: "
 echo localhost> bin\ip.pref
 start /d bin start-logged.bat
