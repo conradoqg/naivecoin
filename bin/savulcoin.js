f973cef7976d2569bbf6addb5d8e8a694f4c7924
LinkNotZelda8 <linknotzelda8@gmail.com>
2021-01-24 00:35:00 +0300

    Auto miner mode

--- bin/savulcoin.js
+++ bin/savulcoin.js
@@ -10,9 +10,11 @@ const argv = require('yargs')
     .alias('l', 'log-level')
     .describe('l', 'Log level (7=dir, debug, time and trace; 6=log and info; 4=warn; 3=error, assert; 6 by default).')
     .describe('peers', 'Peers list.')
+    .array('peers')
     .describe('name', 'Node name/identifier.')
     .describe('proxyUrl', 'URL of the proxy. (null by default)')
-    .array('peers')
+    .describe('minerMode', 'Mine automatically forever (false by default)')
+    .describe('rewardAddress', 'Miner mode reward address')
     .help('h')
     .alias('h', 'help')
     .argv;
@@ -22,4 +24,5 @@ const DEFAULT_PEERS = [
     "savul-n2.yapsavun.com"
 ]
 
-savulcoin(argv.host, argv.port, argv.peers !== undefined ? [...argv.peers, ...DEFAULT_PEERS] : DEFAULT_PEERS, argv.logLevel, argv.name, argv.proxyUrl);
+let peers = argv.peers !== undefined ? [...argv.peers, ...DEFAULT_PEERS] : DEFAULT_PEERS;
+savulcoin(argv.host, argv.port, peers, argv.logLevel, argv.name, argv.proxyUrl, argv.minerMode, argv.rewardAddress);
