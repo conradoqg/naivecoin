056a7f01394981280a7ff4add87dcb017a4ceacf
LinkNotZelda8 <linknotzelda8@gmail.com>
2021-01-27 15:09:22 +0300

    Nexe build

---
+++ nexe-build.sh
@@ -0,0 +1,6 @@
+sudo npm i -g nexe@3.3.7
+nexe bin/savulcoin.js --output ./build/savulcoin-linux-x64 --target linux-x64-14.15.3
+nexe bin/savulcoin.js --output ./build/savulcoin-linux-x86 --target linux-x86-14.15.3
+nexe bin/savulcoin.js --output ./build/savulcoin-windows-x64.exe --target windows-x64-14.15.3
+nexe bin/savulcoin.js --output ./build/savulcoin-windows-x86.exe --target windows-x86-14.15.3
+nexe bin/savulcoin.js --output ./build/savulcoin-mac-x64 --target mac-x64-14.15.3
